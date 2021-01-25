f3d0740e131b2ae7e31da00b22d0479d0a51555c
ahsanhyder <ahsan@sugarcosmetics.com>
2021-01-25 11:47:46 +0530

    product1 commit

---
+++ productpagesugar/pages/product1.js
@@ -0,0 +1,151 @@
+import Head from 'next/head'
+import {useState} from 'react'
+import axios from 'axios'
+
+export default function product1    ({data}){
+    const [demoFlag, setdemoFlag] = useState(false)
+    const [demoFlag1, setdemoFlag1] = useState(false)
+    const [productData, setProductData] = useState(data)
+    const [imgData, setimgData] = useState(productData.resbody.variants[0].images)
+    const [price,setprice] = useState(productData.resbody.variants[0].price)
+    const [offerText,setofferText] = useState(productData.resbody.variants[0].offers)
+    // console.log(productData.resbody.title)
+    // console.log(productData.resbody.variants[0].images)
+    // console.log(productData.resbody.body_html)
+    // console.log(productData.resbody.youtube_id)
+    console.log(productData.resbody.variants[0].offers)
+
+    return(
+        <>
+        <div>
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+        </div>
+        <div class="container-fluid mt-3 mb-3">
+            <div class="row">
+                <div class="col-4 col-sm-3 col-md-4  "></div>
+                <div class="col-6 col-sm-7 col-md-4 col-lg-3">
+                <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></li>
+    <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
+    <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
+  </ol>
+  
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src={imgData[0]} class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src={imgData[1]} class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src={imgData[2]} class="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </a>
+</div>
+                </div>
+                <div class="col-2 col-sm-2 col-md-4"></div>
+            </div>
+            <div class="row">
+                <div class="col text-center">
+                <h3>{productData.resbody.title}</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col text-center">
+                <h5>Rs. {price}</h5>                
+                </div>
+            </div>
+        </div>
+
+        <div class="container-fluid mx-4">
+            <div class="row">
+                <div class="col">
+                <h5 class="text-success">AVAILABLE OFFERS</h5>
+                </div>
+            </div>
+<div>
+    {offerText.map((ele)=>{
+        return(
+            <div>
+                <div>{ele.productOfferText}</div>
+            </div>
+        )
+    })}
+</div>
+            <div class="row mt-3">
+                <div class="col text-center">
+                    <h5>Delivery details</h5>
+                </div>
+                <div class="col text-center">
+                <input class="text-center" type="text" placeholder="Enter Pincode"/>
+                </div>
+                <div class="col">
+                <button type="button" class="btn btn-outline-secondary btn-block">Check</button>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid mx-4">
+            <div class="row">
+                <div class="col">
+                    <img  src="https://cdn.shopify.com/s/files/1/0906/2558/files/SUGAR_Trust_Seal_473x.progressive.jpg?v=1597728764" alt=""/>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid mx-4">
+            <div class="row">
+                <div class="col">
+                <h5 class="text-success">PRODUCT DESCRIPTION</h5>
+                </div>
+            </div>
+           <div dangerouslySetInnerHTML={{__html:[productData.resbody.body_html]}}></div>
+            </div> 
+            {productData.resbody.youtube_id&&  
+        <div class="container">
+                <div class="ml-lg-5 d-none d-md-block">
+                <iframe class="bye" width="100%" height="250px" src={`https://www.youtube.com/embed/${productData.resbody.youtube_id}`}frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                </div>
+                <div class=" d-block d-md-none " style={{marginLeft:"100px  "}}>
+                <iframe class="hi" width="100%" height="250px" src={`https://www.youtube.com/embed/${productData.resbody.youtube_id}`} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                </div>
+        </div>}
+        </>
+        
+    )
+}
+
+export async function getStaticProps(){
+
+    var axios = require('axios');
+    
+    var config = {
+      method: 'get',
+      url: 'https://qa.api.sugarcosmetics.com/products/qa/getProductsv2?handle=beginners-must-have-kit',
+      headers: { }
+    };
+    
+    let data = await axios(config)
+    .then(function (response) {
+    return response.data
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+    return {
+        props: {
+            data
+        }
+    }
+}
